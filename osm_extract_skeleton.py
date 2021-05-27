bead98fe90b3417118369d65044b980f9d6ca639
Debo790 <andrea.debeni@studenti.unitn.it>
2021-05-27 19:07:06 +0200

    terrible but it works. going to improve this

--- osm_extract_skeleton.py
+++ osm_extract_skeleton.py
@@ -2,8 +2,10 @@ import geopandas as gpd
 from osm2geojson.helpers import overpass_call
 from osm2geojson.main import xml2geojson
 
+city = "(area[\"name:it\"=\"Roma\"];)->.searchArea;"
+
 myquery = '''
-    area[name="Suzzara"]->.searchArea;
+    {}
     (
     nwr["sport"](area.searchArea);
     );
@@ -11,10 +13,27 @@ myquery = '''
     out body;
     (._;>;);
     out skel qt;
-    '''
+    '''.format(city)
 
 result = overpass_call(myquery)
 res = xml2geojson(result)
 
 
 gdf = gpd.GeoDataFrame.from_features(res)
+if len(gdf)==0:
+    print(city)
+    city = city.replace("\"name:it\"", "name")
+    myquery = '''
+        {}
+        (
+        nwr["sport"](area.searchArea);
+        );
+        out geom;
+        out body;
+        (._;>;);
+        out skel qt;
+        '''.format(city)
+    result = overpass_call(myquery)
+    res = xml2geojson(result)
+    gdf = gpd.GeoDataFrame.from_features(res)
+print(len(gdf))
