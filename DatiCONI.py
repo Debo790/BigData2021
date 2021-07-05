91367d1f934463732afa805b7b46dbba6fa50dce
Debo790 <andrea.debeni@studenti.unitn.it>
2021-07-05 19:13:27 +0200

    added redis

--- DatiCONI.py
+++ DatiCONI.py
@@ -1,20 +1,21 @@
 import argparse
-from pandas.io.stata import excessive_string_length_error
 import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
 import time
+import redis
 from db_wrapper import PostgresDB
 
 class ConiExtractor:
 
     
-    def __init__(self) -> None:
-        self.codici = []    # codici di tutte le societa iscritte al Coni
+    def __init__(self, r: redis.Redis) -> None:
+        self.codici = set()    # codici di tutte le societa iscritte al Coni
         self.dati = []      # lista di dizionari con i dettagli delle società (utilizzato per debug)
         self.societa = pd.DataFrame()
         self.db = PostgresDB()
+        self.r = r
 
     
     def extractSocietiesCode(self):
@@ -43,17 +44,19 @@ class ConiExtractor:
                     for element in results:
                         if len(element)>1:
                             numero = str(element).split()[2]                # link società
-                            numero=numero.strip('">')               
-                            self.codici.append(numero.split('=')[-1])
-                            #print("In results... Association no. {}".format(el))
-                            el+=1
+                            numero=numero.strip('">')           
+                            if not self.r.sismember("coni:added", numero.split('=')[-1]):
+                                self.codici.add(numero.split('=')[-1])
+                                self.r.sadd("coni:code", numero.split('=')[-1])
+                                #print("In results... Association no. {}".format(el))
+                                el+=1
                 else:
                     print("Skipped last one, no results retrieved.")
 
-                if(len(self.codici)%2000 == 0):
+                if(len(self.codici)%2000 == 0 and len(self.codici) != 0):
                     # Wait to let Coni's website breathe
-                    print("20 seconds waiting...")
-                    time.sleep(20)
+                    print("5 seconds waiting...")
+                    time.sleep(5)
 
                 print("Parsed {} associations. Elapsed time: {} s".format(len(self.codici), round(time.time()-ti, 2)))
                 print("-----------------------------------------------")
@@ -63,63 +66,70 @@ class ConiExtractor:
         #---------------------------------------------------------------------------------------------------
 
             
-    def extractSocietiesData(self, to_json: bool):
+    def extractSocietiesData(self, to_json: bool, timeout: int):
 
         # ESTRAZIONE DATI SOCIETA'--------------------------------------------------------------------------
         print("Extracting associations data.....")
         baseURL="https://www.coni.it/it/registro-societa-sportive/home/registro-2-0/registro_dettaglio.html?id_societa="
 
         ti = time.time()
-        for codice in self.codici:
+        for codice in self.r.smembers("coni:code"):
+            codice = codice.decode("utf-8")
             indirizzo=baseURL+str(codice)
             try:
-                page = requests.get(indirizzo, timeout=10)
-            except:
-                print("Connection timed out for society with code {}. Society discarded.".format(codice))
+                page = requests.get(indirizzo, timeout=timeout)
+                self.r.srem("coni:code", codice)
+                self.r.sadd("coni:added", codice)
+            
+                if page.ok:
 
-            if page.ok:
+                    #print(page.content)
+                    soup = BeautifulSoup(page.content, 'html.parser')
+                    #trovo la parte della pagina che mi interessa:
+                    results = soup.find('section', id='component')
+                    #print(results.prettify())
 
-                #print(page.content)
-                soup = BeautifulSoup(page.content, 'html.parser')
-                #trovo la parte della pagina che mi interessa:
-                results = soup.find('section', id='component')
-                #print(results.prettify())
+                    if results is not None:
+                        societa_luogo = results.find('div', class_='dati-descrittivi')
+                        name=societa_luogo.find('h3', class_='nome').text
+                        regione=societa_luogo.find('p', class_='regione').text
+                        provincia=societa_luogo.find('span', class_='provincia').text
+                        comune=(societa_luogo.find('p', class_='comune').text).split()[0]
 
-                if results is not None:
-                    societa_luogo = results.find('div', class_='dati-descrittivi')
-                    name=societa_luogo.find('h3', class_='nome').text
-                    regione=societa_luogo.find('p', class_='regione').text
-                    provincia=societa_luogo.find('span', class_='provincia').text
-                    comune=(societa_luogo.find('p', class_='comune').text).split()[0]
+                        #print("Adding: {}".format(name))
 
-                    #print("Adding: {}".format(name))
+                        societa_numeri=results.find('div', class_='dati-container')
+                        agonistici_praticanti=[societa_numeri('span', class_='numero')[2].text, societa_numeri('span', class_='numero')[3].text]
 
-                    societa_numeri=results.find('div', class_='dati-container')
-                    agonistici_praticanti=[societa_numeri('span', class_='numero')[2].text, societa_numeri('span', class_='numero')[3].text]
+                        dettaglio = {'code': codice,
+                        'name': name,
+                        'region': regione,
+                        'province': provincia,
+                        'comune': comune,
+                        'agonistici': agonistici_praticanti[0],
+                        'praticanti': agonistici_praticanti[1]
+                        }
+                        
+                        self.societa = self.societa.append(dettaglio, ignore_index=True)
+                        if to_json:
+                            self.dati.append(dettaglio)
+                    
+                    else:
+                        print("Issue in retrieving data, skipped.")
 
-                    dettaglio = {'code': codice,
-                    'name': name,
-                    'region': regione,
-                    'province': provincia,
-                    'comune': comune,
-                    'agonistici': agonistici_praticanti[0],
-                    'praticanti': agonistici_praticanti[1]
-                    }
-                    self.dati.append(dettaglio)
-                    self.societa = self.societa.append(dettaglio, ignore_index=True)
-                
-                else:
-                    print("Issue in retrieving data, skipped.")
+                    if(len(self.societa)%200 == 0):
+                        print("Waiting 5 sec. Requested data for {} societies.".format(len(self.societa)))
+                        time.sleep(5)
 
-                if(len(self.societa)%200 == 0):
-                    print("Waiting 20 sec. Requested data for {} societies.".format(len(self.societa)))
-                    time.sleep(20)
+                    if(len(self.societa)%1000 == 0):
+                        self.load(self.societa)
+                        self.societa = pd.DataFrame()
+                        print("Time elapsed for the bulk: {} s.".format(round(time.time()-ti, 2)))
+                        ti = time.time()
 
-                if(len(self.societa)%1000 == 0):
-                    self.load(self.societa)
-                    self.societa = pd.DataFrame()
-                    print("Time elapsed for the bulk: {} s.".format(round(time.time()-ti, 2)))
-                    ti = time.time()
+            except requests.exceptions.Timeout:
+                print("Connection timed out for association with code {}. Association discarded.".format(codice))
+                self.r.sadd("coni:discarded", codice)
 
         self.load(self.societa) # Last upload
         self.societa = pd.DataFrame()
@@ -133,27 +143,34 @@ class ConiExtractor:
 
     def load(self, societa: pd.DataFrame):
         if self.db.insert_df(societa, "coni"):
-            print("Inserted {} societies.".format(len(societa)))
+            print("Inserted {} associations.".format(len(societa)))
 
 
-    def run(self, to_json: bool) -> bool:
+    def run(self, to_json: bool, timeout: int) -> bool:
         self.extractSocietiesCode()
-        self.extractSocietiesData(to_json)
+        self.extractSocietiesData(to_json, timeout)
+        while self.r.scard("coni:discarded") > 0 and timeout<=30:
+            timeout = timeout+10
+            print("Round completed. Timeout increased to {}s. Missing {} associations.".format(timeout, self.r.scard("coni:discarded")))
+            self.r.delete("coni:discarded")
+            self.extractSocietiesData(to_json, timeout)
         return True
 
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="BDT Project 2021 - CONI societies data extraction")
+    parser = argparse.ArgumentParser(description="BDT Project 2021 - CONI associations data extraction")
     parser.add_argument("--to_json", help="Write results in tmp/dati_CONI.json (debug purpose only)")
     args = parser.parse_args()
 
-    extractor = ConiExtractor()
+    r = redis.Redis(host="localhost", port=6379, db=0)
+
+    extractor = ConiExtractor(r)
     success = False
     if args.to_json:
-        success = extractor.run(to_json=True)
+        success = extractor.run(to_json=True, timeout=10)
     else:
-        success = extractor.run(to_json=False)
+        success = extractor.run(to_json=False, timeout=10)
 
     if success:
-        print("All societes data extracted and loaded.")
+        print("Associations data extracted and loaded.")
